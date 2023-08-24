d63c79fb599c58b26410be73ee3678b1f58ec38a
CaiLi <dy5025115@gmail.com>
2023-08-24 21:31:24 +0800

    update text

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 80;
 
 app.get("/", (req, res) => {
   res.send(
-    "hi This project was used to demonstrate Github Action deployment of AWS ECR to implement the CI/CD pipeline. AAASSSSSS"
+    "111hi This project was used to demonstrate Github Action deployment of AWS ECR to implement the CI/CD pipeline. AAASSSSSS"
   );
 });
 
