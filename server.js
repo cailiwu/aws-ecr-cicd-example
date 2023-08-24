4b226374642294a0b854aa8f5ee4700ccc41f59b
CaiLi <dy5025115@gmail.com>
2023-08-24 21:27:21 +0800

    update text

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 80;
 
-app.get('/', (req, res) => {
-  res.send('This project was used to demonstrate Github Action deployment of AWS ECR to implement the CI/CD pipeline.');
+app.get("/", (req, res) => {
+  res.send(
+    "This project was used to demonstrate Github Action deployment of AWS ECR to implement the CI/CD pipeline. AAASSSSSS"
+  );
 });
 
 app.listen(PORT, () => {
